baad52bffb7037148d7920b3b1097fc9bd61f8f3
dhruvnagar01 <iitscooldhruv@gmail.com>
2023-10-12 15:47:56 +0530

    filter option done

--- main1.js
+++ main1.js
@@ -1,10 +1,13 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
     // Delete event
     itemList.addEventListener('click', removeItem);
+//filter event
+filter.addEventListener('keyup',filteritems);
 
     // Add Item
 
@@ -55,5 +58,24 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+//FILTER ITEMS
+
+function filteritems(e){
+    //convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // GET lis 
+    var items = itemList.getElementsByTagName('li');
     
+    //convert to an array
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text)!= -1){
+            item.style.display = 'block';
+        }
+        else {
+            item.style.display = 'none';
+        }
+    });
 }
