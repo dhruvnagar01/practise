f78337d2eb85999f34f6d99f21882c1e73419561
Dhruv Nagar <112823969+dhruvnagar01@users.noreply.github.com>
2023-10-19 16:45:11 +0530

    Update main.js
    AXIOS "post" and 'get" updated

--- project1/main.js
+++ project1/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('myForm');
+window.loadData();
 
 
 form.addEventListener('submit', function(e) {
@@ -15,20 +16,41 @@ form.addEventListener('submit', function(e) {
         phone : phone.value
     };
 
-    localStorage.setItem(user.email, JSON.stringify(user));
+    // localStorage.setItem(user.email, JSON.stringify(user));
+    axios.post("https://crudcrud.com/api/ae4a964828be4c08b9d42d3ce6ece679/createData", user).then((result) => {
+        
+        toshow(result.data);
+    }).catch((err) => {
+     console.log(err);   
+    });
 
-    toshow(user);
+    
 });
 
+function loadData() {
+        axios.get("https://crudcrud.com/api/ae4a964828be4c08b9d42d3ce6ece679/createData").then((result) => {
+            result.data.forEach(element => {
+                toshow(element);        
+                console.log(element);
+            });
+        
+       
+    }).catch((error) => console.log(error));
+
+}
+
 function toshow(obj){
+      
     
    const parentElem = document.getElementById('listOfItems');
    const childElem = document.createElement('li');
 
-   //DELETE BUTTON
+//    DELETE BUTTON
    var deltebtn = document.createElement('button');
    deltebtn.appendChild(document.createTextNode('Delete'));
+
    childElem.textContent = obj.name + ' - ' + obj.email + ' - ' + obj.phone;
+
    childElem.appendChild(deltebtn);
 
    //EDIT BUTTON
@@ -36,38 +58,41 @@ function toshow(obj){
    editbtn.appendChild(document.createTextNode('edit'));
    childElem.appendChild(editbtn);
     
-   // add both in li
+//    // add both in li
    parentElem.appendChild(childElem);
    
    // delte button function
 deltebtn.addEventListener('click', (e)=> {
       e.preventDefault();
   
-      localStorage.removeItem(obj.email);
+     axios.delete("https://crudcrud.com/api/ae4a964828be4c08b9d42d3ce6ece679/createData",obj.id).then((result)=>
+     {  console.log(result)
+        })
+     .catch((error)=>console.log(error));
       var li = e.target.parentElement;
       parentElem.removeChild(li);
   
   });
 
 
-  editbtn.addEventListener('click', (e)=>{
-    e.preventDefault();
+//   editbtn.addEventListener('click', (e)=>{
+//     e.preventDefault();
 
-    let user = localStorage.getItem(obj.email);
-    let userData = JSON.parse(user);
+//     let user = localStorage.getItem(obj.email);
+//     let userData = JSON.parse(user);
     
-    let name = form.elements['name'];
-    let email = form.elements['email'];
-    let phone = form.elements['phone'];
+//     let name = form.elements['name'];
+//     let email = form.elements['email'];
+//     let phone = form.elements['phone'];
 
-    name.value = userData.name;
-    email.value = userData.email;
-    phone.value = userData.phone;
+//     name.value = userData.name;
+//     email.value = userData.email;
+//     phone.value = userData.phone;
         
-    //deleting list
-    localStorage.removeItem(obj.email);
-      var li = e.target.parentElement;
-      parentElem.removeChild(li);
-});
+//     //deleting list
+//     localStorage.removeItem(obj.email);
+//       var li = e.target.parentElement;
+//       parentElem.removeChild(li);
+// });
     
 }
