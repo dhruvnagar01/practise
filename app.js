c72b57ee0814b8ad4f49320ffd4394a56015a350
Dhruv Nagar <112823969+dhruvnagar01@users.noreply.github.com>
2023-11-11 17:52:51 +0530

    add product module added

--- app.js
+++ app.js
@@ -11,9 +11,13 @@ app.use('/', (req, res, next) => {
 
 app.use('/add-product', (req, res, next) => {
 console.log('In the middleware add product');
-res.send('<h1>The Add Product</h1>');
+res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
 app.use('/', (req, res, next) => {
     console.log('In the middleware');
