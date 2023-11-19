9f036b61900dfa85368a9726fedc2c4e99c1d292
Dhruv Nagar <112823969+dhruvnagar01@users.noreply.github.com>
2023-11-19 18:29:54 +0530

    server SQL added

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -3,7 +3,10 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
+
+const db = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,12 +15,20 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+db.execute('SELECT * FROM products').
+then(result => {
+    console.log(result);
+})
+.catch(err => {
+    console.log(err);
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.get404 );
+app.use(errorController.get404);
 
-app.listen(3000);
+app.listen(4000);
