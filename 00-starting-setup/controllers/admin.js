22004c51d87f6b7044e08d363c2d580707d1dfe1
Dhruv Nagar <112823969+dhruvnagar01@users.noreply.github.com>
2023-11-21 00:07:09 +0530

    data fetch from DB
    Delete code added also

--- 00-starting-setup/controllers/admin.js
+++ 00-starting-setup/controllers/admin.js
@@ -14,9 +14,15 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product
+  .save()
+  .then((result) => {
+    res.redirect('/');
+  })
+  
+  .catch(err => console.log(err));
 };
+
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
@@ -65,7 +71,6 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.deleteById(prodId);
+  Product.deleteById(prodId).then(result => console.log("data delete success")).catch(err=> console.log(err));
   res.redirect('/admin/products');
 };
